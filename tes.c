41f078c393409f63e9abaab2302d0e5ad0f1928c
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-11-08 22:36:03 +0800

    menambah fungsi pertambahan int

--- tes.c
+++ tes.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
-    int tes;
-    printf ("hello world");
-    printf ("ini tes lagi");   
-    scanf ("%d", &tes);
-    printf ("test tes lagi %d\n",tes);
-    printf ("test tes lagi");
-    return 0;
+int pertambahan(int a, int b)
+{
+    // int c = a + b;
+    // printf("%d", c);
+    printf("\n\nIni Kode Bisma");
+    printf("\n--------------------");
+    printf("\n%d + %d = %d", a, b, a + b);
+}
 
+int main()
+{
+    int tes;
+    printf("hello world");
+    printf("ini tes lagi");
+    scanf("%d", &tes);
+    printf("test tes lagi %d\n", tes);
+    printf("test tes lagi");
+    pertambahan(10, 9);
+    return 0;
 }

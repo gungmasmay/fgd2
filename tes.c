a75ce1746231d7e66cfe0fccf62b98785e960fd6
Gung Mas May <gungmas6@gmail.com>
2024-11-08 22:12:21 +0800

    tes1

---
+++ tes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int tes;
+    printf ("hello world");
+    printf ("ini tes lagi");   
+    scanf ("%d", &tes);
+    printf ("test tes lagi %d\n",tes);
+    return 0;
+}
